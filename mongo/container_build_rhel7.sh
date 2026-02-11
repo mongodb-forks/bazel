2c3dfca39576effcb54a444e55c22b7e2d833137
Charanya Sudharsanan <charanya.sudharsanan@mongodb.com>
2026-02-11 10:27:59 -0800

    Add Evergreen build configuration for 8.3.0-mongo

---
+++ mongo/container_build_rhel7.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o errexit
+set -o verbose
+
+yum install -y rh-python36 zip unzip
+export CC=$(which gcc)
+curl -L "$1" -o bazel_bootstrap
+chmod +x ./bazel_bootstrap
+scl enable rh-python36 './bazel_bootstrap build  --compilation_mode=opt --subcommands --verbose_failures --stamp --embed_label=$2 //src:bazel'
+cp bazel-bin/src/bazel "$3"
+sha256sum -b "$3" > "${3}.sha256"

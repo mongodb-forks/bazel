c373bd1f4fee74d257c699edf98a555702d459d7
Zac Codiamat <zac.codiamat@mongodb.com>
2026-05-07 17:20:02 +0000

    fix localfirst fallback download

--- src/main/java/com/google/devtools/build/lib/remote/downloader/GrpcRemoteDownloader.java
+++ src/main/java/com/google/devtools/build/lib/remote/downloader/GrpcRemoteDownloader.java
@@ -183,7 +183,8 @@ public class GrpcRemoteDownloader implements AutoCloseable, Downloader {
             destination,
             eventHandler,
             clientEnv,
-            type);
+            type,
+            context);
         return;
       } catch (IOException e) {
         eventHandler.handle(
